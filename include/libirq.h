5a30830013965e54486cf881dbc5118b8f4de148
Keeley Hoek <keeley@hoek.io>
2021-08-06 06:17:46 +1000

    Update

--- include/libirq.h
+++ include/libirq.h
@@ -1,5 +1,4 @@
-#ifndef __LIB_LIBIRQ_H
-#define __LIB_LIBIRQ_H
+#pragma once
 
 #include <driver/gpio.h>
 #include <esp_err.h>
@@ -14,50 +13,56 @@ typedef struct libirq_waiter {
     libirq_source_handle_t __internal;
 } libirq_waiter_t;
 
-// Creates an IRQ source on the given GPIO pin and with the specified level, which is
-// processed on the specified core. In order to use the generated `irq_source_handle_t`
-// you must create one or more waiters using `libirq_waiter_sleep_until_active()`. Each
-// waiter behaves identically, but generally cannot be shared between threads---if a call to
-// `libirq_waiter_sleep_until_active()` ever returns `false` it may never be used again
-// (this only happens when the underlying IRQ source has been destroyed), and therefore
-// it is unsafe to share a single waiter between threads where one thread could pass the
-// waiter to `libirq_waiter_sleep_until_active()` after the same function call has returned
-// `false` on another thread.
-esp_err_t libirq_source_create(gpio_num_t pin_irq, bool trigger_high, BaseType_t core_id,
+// Creates an IRQ source on the given GPIO pin and with the specified level,
+// which is processed on the specified core. In order to use the generated
+// `irq_source_handle_t` you must create one or more waiters using
+// `libirq_waiter_sleep_until_active()`. Each waiter behaves identically, but
+// generally cannot be shared between threads---if a call to
+// `libirq_waiter_sleep_until_active()` ever returns `false` it may never be
+// used again (this only happens when the underlying IRQ source has been
+// destroyed), and therefore it is unsafe to share a single waiter between
+// threads where one thread could pass the waiter to
+// `libirq_waiter_sleep_until_active()` after the same function call has
+// returned `false` on another thread.
+esp_err_t libirq_source_create(gpio_num_t pin_irq, bool trigger_high,
+                               BaseType_t core_id,
                                libirq_source_handle_t* out_handle);
 
-// This function may be called at most once, and when it returns it guarentees that any
-// associated `irq_waiter_t` handles will return false upon a subsequent call to
-// `libirq_waiter_sleep_until_active()`. (Thus the waiter handles will also then be destroyed.)
+// This function may be called at most once, and when it returns it guarentees
+// that any associated `irq_waiter_t` handles will return false upon a
+// subsequent call to `libirq_waiter_sleep_until_active()`. (Thus the waiter
+// handles will also then be destroyed.)
 //
 // It is illegal to use `handle` after a call to this function.
 void libirq_source_destroy(libirq_source_handle_t handle);
 
-// Triggers an IRQ event for the given IRQ soruce, exactly as if a real IRQ had occured
-// (thereby waking all currently sleeping waiters).
+// Triggers an IRQ event for the given IRQ soruce, exactly as if a real IRQ had
+// occured (thereby waking all currently sleeping waiters).
 void libirq_source_trigger(libirq_source_handle_t handle);
 
-// Creates a waiter for the given `handle`. Used to wait for the IRQ assigned to the source
-// to become active. See the description of `libirq_source_create()` for the purpose of waiters.
+// Creates a waiter for the given `handle`. Used to wait for the IRQ assigned to
+// the source to become active. See the description of `libirq_source_create()`
+// for the purpose of waiters.
 libirq_waiter_t libirq_waiter_create(libirq_source_handle_t handle);
 
-// Explicitly destroys the given waiter. The waiter subsequently cannot be used ever again.
+// Explicitly destroys the given waiter. The waiter subsequently cannot be used
+// ever again.
 void libirq_waiter_destroy(libirq_waiter_t waiter);
 
-// Returns `true` if we woke because the assigned IRQ became active,
-// else returns `false` if the IRQ source has been destroyed.
+// Returns `true` if we woke because the assigned IRQ became active, else
+// returns `false` if the IRQ source has been destroyed.
 //
-// DANGER: If this function returns false it is illegal to use the
-// waiter ever again (including passing the waiter to `libirq_waiter_destroy()`).
+// DANGER: If this function returns false it is illegal to use the waiter ever
+// again (including passing the waiter to `libirq_waiter_destroy()`).
 bool libirq_waiter_sleep_until_active(libirq_waiter_t waiter);
 
-// Convenience method to create a libtask loop task from the given IRQ source waiter, which continuously
-// waits for an active IRQ and calls `do_once()` each time it occurs.
+// Convenience method to create a libtask loop task from the given IRQ source
+// waiter, which continuously waits for an active IRQ and calls `do_once()` each
+// time it occurs.
 //
 // It is illegal to use the given `waiter` after it is passed to this task.
 esp_err_t libirq_managed_loop_spawn(libirq_waiter_t waiter,
                                     libtask_do_once_fn_t do_once, void* private,
-                                    const char* name, uint32_t task_stack_size, UBaseType_t task_priority,
+                                    const char* name, uint32_t task_stack_size,
+                                    UBaseType_t task_priority,
                                     libtask_loop_handle_t* out_handle);
-
-#endif
--- src/libirq/libirq.c
+++ src/libirq/libirq.c
@@ -30,7 +30,8 @@ struct libirq_source {
     uint32_t refcount;
 };
 
-static libirq_source_t* alloc_irq_source(gpio_num_t pin_irq, bool trigger_high) {
+static libirq_source_t* alloc_irq_source(gpio_num_t pin_irq,
+                                         bool trigger_high) {
     libirq_source_t* src = malloc(sizeof(libirq_source_t));
     src->pin_irq = pin_irq;
     src->trigger_high = trigger_high;
@@ -74,58 +75,64 @@ static void dec_refcount_irq_source(libirq_source_t* src) {
 static void isr_irq(void* arg) {
     libirq_source_t* src = arg;
 
-    // First, since this interrupt is level-triggered (so we don't miss any) disable further
-    // interrupts. If the waiting task happens to enable this interrupt again asynchronously
-    // with this, we don't mind, since `sleep_until_irq_active()` clears spurious pending
-    // messages.
+    // First, since this interrupt is level-triggered (so we don't miss any)
+    // disable further interrupts. If the waiting task happens to enable this
+    // interrupt again asynchronously with this, we don't mind, since
+    // `sleep_until_irq_active()` clears spurious pending messages.
     gpio_intr_disable(src->pin_irq);
 
-    // Only now give the semaphore, which will ensure that the `irq_loop()` will always
-    // eventually take the semaphore and call `gpio_intr_enable()` for us. (We don't care
-    // about the return value, i.e. whether we were able to.)
+    // Only now give the semaphore, which will ensure that the `irq_loop()` will
+    // always eventually take the semaphore and call `gpio_intr_enable()` for
+    // us. (We don't care about the return value, i.e. whether we were able to.)
     xEventGroupSetBits(src->events, IRQ_EVENT_GOT_IRQ);
 }
 
 // Returns `true` if the sleep ended because the IRQ became active,
-// else returns `false` if the sleep was interrupted by a call for the underlying IRQ source
-// to destroy itself.
+// else returns `false` if the sleep was interrupted by a call for the
+// underlying IRQ source to destroy itself.
 static bool sleep_until_irq_active(const libirq_source_t* src) {
-    // Clear a potential previous IRQ message from the IRQ ISR. Note that this call
-    // is safe to make even if other tasks are currently waiting on this bit.
+    // Clear a potential previous IRQ message from the IRQ ISR. Note that this
+    // call is safe to make even if other tasks are currently waiting on this
+    // bit.
     //
-    // It is important that this bit is never cleared after a call to `gpio_intr_enable`(),
-    // e.g. upon its reciept below, because this can result in a race condition
-    // (as explained below).
+    // It is important that this bit is never cleared after a call to
+    // `gpio_intr_enable`(), e.g. upon its reciept below, because this can
+    // result in a race condition (as explained below).
     xEventGroupClearBits(src->events, IRQ_EVENT_GOT_IRQ);
 
-    // First, enable the IRQ ISR. This is safe to call if another task has already
-    // done this and is currently waiting (but in principle could cause a "race" where
-    // both the we and the other task observe the "first" IRQ to arrive, while the ISR
-    // triggers again due to this call and sets the `IRQ_EVENT_GOT_IRQ` bit again
-    // "supriously"). This corner-case is dealt with by the bit clear performed above---
-    // it can only occur when we make this `gpio_intr_enable()` call after the ISR
-    // has already triggered and disabled itself, but before the other waiting task has
-    // recieved the `IRQ_EVENT_GOT_IRQ` message and cleared the pending bit.
+    // First, enable the IRQ ISR. This is safe to call if another task has
+    // already done this and is currently waiting (but in principle could cause
+    // a "race" where both the we and the other task observe the "first" IRQ to
+    // arrive, while the ISR triggers again due to this call and sets the
+    // `IRQ_EVENT_GOT_IRQ` bit again "supriously"). This corner-case is dealt
+    // with by the bit clear performed above--- it can only occur when we make
+    // this `gpio_intr_enable()` call after the ISR has already triggered and
+    // disabled itself, but before the other waiting task has recieved the
+    // `IRQ_EVENT_GOT_IRQ` message and cleared the pending bit.
     gpio_intr_enable(src->pin_irq);
 
     // Now wait until the ISR tells us IRQ has gone active again.
     //
-    // Note that it could have happened that after this `gpio_intr_enable()` call, because
-    // of another wait operation in progress on another thread the ISR had already triggered,
-    // disabled itself, and then sent the `IRQ_EVENT_GOT_IRQ` bit. It is therefore imperative
-    // that the other waiting task does not automatically clear the `IRQ_EVENT_GOT_IRQ` bit
-    // upon its reciept. Else this could occur before we call `xEventGroupWaitBits()` ourselves,
-    // thereby missing the setting of `IRQ_EVENT_GOT_IRQ` which we wanted to observe. This would
-    // then lock up this thread until another thread calls `sleep_until_irq_active()` again.
+    // Note that it could have happened that after this `gpio_intr_enable()`
+    // call, because of another wait operation in progress on another thread the
+    // ISR had already triggered, disabled itself, and then sent the
+    // `IRQ_EVENT_GOT_IRQ` bit. It is therefore imperative that the other
+    // waiting task does not automatically clear the `IRQ_EVENT_GOT_IRQ` bit
+    // upon its reciept. Else this could occur before we call
+    // `xEventGroupWaitBits()` ourselves, thereby missing the setting of
+    // `IRQ_EVENT_GOT_IRQ` which we wanted to observe. This would then lock up
+    // this thread until another thread calls `sleep_until_irq_active()` again.
     //
-    // As a solution, we simply don't ask FreeRTOS to automatically clear the `IRQ_EVENT_GOT_IRQ`
-    // bit upon its reciept. Instead we perform the relevant bit clear above, before the call to
-    // `gpio_intr_enable()`, which solves all of our propblems.
+    // As a solution, we simply don't ask FreeRTOS to automatically clear the
+    // `IRQ_EVENT_GOT_IRQ` bit upon its reciept. Instead we perform the relevant
+    // bit clear above, before the call to `gpio_intr_enable()`, which solves
+    // all of our propblems.
     EventBits_t bits;
     do {
-        bits = xEventGroupWaitBits(src->events,
-                                   (IRQ_EVENT_GOT_IRQ | IRQ_EVENT_SHOULD_TEARDOWN),
-                                   false, false, portMAX_DELAY);
+        bits =
+            xEventGroupWaitBits(src->events,
+                                (IRQ_EVENT_GOT_IRQ | IRQ_EVENT_SHOULD_TEARDOWN),
+                                false, false, portMAX_DELAY);
     } while (!(bits & (IRQ_EVENT_GOT_IRQ | IRQ_EVENT_SHOULD_TEARDOWN)));
 
     return !(bits & IRQ_EVENT_SHOULD_TEARDOWN);
@@ -137,7 +144,8 @@ static void irq_task(void* arg) {
     gpio_num_t pin_irq = src->pin_irq;
 
     gpio_config_t io_conf = {
-        .intr_type = src->trigger_high ? GPIO_INTR_HIGH_LEVEL : GPIO_INTR_LOW_LEVEL,
+        .intr_type =
+            src->trigger_high ? GPIO_INTR_HIGH_LEVEL : GPIO_INTR_LOW_LEVEL,
         .mode = GPIO_MODE_INPUT,
         .pin_bit_mask = (1ULL << pin_irq),
         .pull_down_en = 0,
@@ -157,8 +165,7 @@ static void irq_task(void* arg) {
 
     EventBits_t bits;
     do {
-        bits = xEventGroupWaitBits(src->events,
-                                   IRQ_EVENT_SHOULD_TEARDOWN,
+        bits = xEventGroupWaitBits(src->events, IRQ_EVENT_SHOULD_TEARDOWN,
                                    false, false, portMAX_DELAY);
     } while (!(bits & IRQ_EVENT_SHOULD_TEARDOWN));
 
@@ -175,18 +182,24 @@ static void irq_task(void* arg) {
     xEventGroupSetBits(src->events, IRQ_EVENT_TEARDOWN_FINISHED);
 }
 
-esp_err_t libirq_source_create(gpio_num_t pin_irq, bool trigger_high, BaseType_t core_id,
+esp_err_t libirq_source_create(gpio_num_t pin_irq, bool trigger_high,
+                               BaseType_t core_id,
                                libirq_source_handle_t* out_handle) {
     libirq_source_t* src = alloc_irq_source(pin_irq, trigger_high);
 
-    // Note that this task will allocate an interrupt, and therefore must happen on a well-defined core
-    // (hence `xTaskCreatePinnedToCore()`), since interrupts must be deallocated on the same core they
-    // were allocated on.
+    char name_buff[32];
+    snprintf(name_buff, sizeof(name_buff), "libirq(%u-%s)", pin_irq,
+             trigger_high ? "HIGH" : "LOW");
+
+    // Note that this task will allocate an interrupt, and therefore must happen
+    // on a well-defined core (hence `xTaskCreatePinnedToCore()`), since
+    // interrupts must be deallocated on the same core they were allocated on.
     //
-    // After allocating the interrupt the task will put itself to sleep, only to awake when it is time
-    // for the interrupt to be deallocated.
-    BaseType_t result = xTaskCreatePinnedToCore(&irq_task, "libirq_task", IRQ_TASK_STACK_SIZE, (void*) src,
-                                                IRQ_TASK_PRIORITY, NULL, core_id);
+    // After allocating the interrupt the task will put itself to sleep, only to
+    // awake when it is time for the interrupt to be deallocated.
+    BaseType_t result =
+        xTaskCreatePinnedToCore(&irq_task, name_buff, IRQ_TASK_STACK_SIZE,
+                                (void*) src, IRQ_TASK_PRIORITY, NULL, core_id);
     if (result != pdPASS) {
         free_irq_source(src);
 
@@ -211,8 +224,7 @@ void libirq_source_destroy(libirq_source_handle_t handle) {
 
     EventBits_t bits;
     do {
-        bits = xEventGroupWaitBits(handle->events,
-                                   IRQ_EVENT_SHOULD_TEARDOWN,
+        bits = xEventGroupWaitBits(handle->events, IRQ_EVENT_SHOULD_TEARDOWN,
                                    false, false, portMAX_DELAY);
     } while (!(bits & IRQ_EVENT_TEARDOWN_FINISHED));
 
@@ -220,8 +232,8 @@ void libirq_source_destroy(libirq_source_handle_t handle) {
 }
 
 libirq_waiter_t libirq_waiter_create(libirq_source_handle_t handle) {
-    // It is a race condition to create waiters after the source has been destroyed.
-    // Also, this is pointless.
+    // It is a race condition to create waiters after the source has been
+    // destroyed. Also, this is pointless.
     assert(!(xEventGroupGetBits(handle->events) & IRQ_EVENT_SHOULD_TEARDOWN));
 
     libirq_waiter_t waiter = {
@@ -270,14 +282,17 @@ static libtask_disposition_t libirq_do_once_wrapper(waiter_loop_data_t* data) {
 
 esp_err_t libirq_managed_loop_spawn(libirq_waiter_t waiter,
                                     libtask_do_once_fn_t do_once, void* private,
-                                    const char* name, uint32_t task_stack_size, UBaseType_t task_priority,
+                                    const char* name, uint32_t task_stack_size,
+                                    UBaseType_t task_priority,
                                     libtask_loop_handle_t* out_handle) {
     waiter_loop_data_t* data = malloc(sizeof(waiter_loop_data_t));
     data->waiter = waiter;
     data->do_once = do_once;
     data->private = private;
 
-    esp_err_t ret = libtask_loop_spawn((libtask_do_once_fn_t) libirq_do_once_wrapper, data, name, task_stack_size, task_priority, out_handle);
+    esp_err_t ret =
+        libtask_loop_spawn((libtask_do_once_fn_t) libirq_do_once_wrapper, data,
+                           name, task_stack_size, task_priority, out_handle);
 
     if (ret != ESP_OK) {
         libirq_waiter_destroy(waiter);
